c225ff5c9213b9063a9c1316094d6020fd570984
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-09-03 19:54:30 -0500

    Añadiendo eventos a movies y movies add, Desafío 1 Listo

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,12 +1,17 @@
-window.onload = function(){
-    let titulo = document.querySelector('.moviesAddTitulo')
-    let formulario = document.querySelector('#formulario');
-    let article = document.querySelector('article');
-    titulo.innerHTML = 'AGREGAR PELÍCULA';
-    titulo.classList.add('titulo');
-    article.classList.add('fondoTransparente');
-    formulario.classList.add('fondoCRUD');
+window.onload = function () {
+	let titulo = document.querySelector('.moviesAddTitulo');
+	let formulario = document.querySelector('#formulario');
+	let article = document.querySelector('article');
+	titulo.innerHTML = 'AGREGAR PELÍCULA';
+	titulo.classList.add('titulo');
+	article.classList.add('fondoTransparente');
+	formulario.classList.add('fondoCRUD');
 
+	titulo.addEventListener('mouseenter', function () {
+		titulo.style.color = 'blue';
+	});
 
-
-}
+	titulo.addEventListener('mouseleave', function () {
+		titulo.style.color = 'black';
+	});
+};
--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,17 +1,16 @@
-window.onload = function(){
-    let body = document.querySelector('body');
-    let moviesListTitulo = document.querySelector('.moviesListTitulo');
+window.onload = function () {
+	let body = document.querySelector('body');
+	let moviesListTitulo = document.querySelector('.moviesListTitulo');
+	let figure = document.querySelector('figure');
 
-    let modo = confirm('Desea modo oscuro');
-    if(modo){
-        body.style.backgroundColor = '#7f7f7f'
-        body.classList.add('fondoMoviesList');
-    }
-    
-    console.log(body);
-    moviesListTitulo.innerHTML = 'LISTADO DE PELÍCULAS';
-    moviesListTitulo.style.color ='white';
-    moviesListTitulo.style.backgroundColor = 'teal';
-    moviesListTitulo.style.padding = '20px';
+	figure.addEventListener('mouseenter', function () {
+		body.style.backgroundColor = '#7f7f7f';
+		// body.classList.add('fondoMoviesList');
+	});
 
-}
+	console.log(body);
+	moviesListTitulo.innerHTML = 'LISTADO DE PELÍCULAS';
+	moviesListTitulo.style.color = 'white';
+	moviesListTitulo.style.backgroundColor = 'teal';
+	moviesListTitulo.style.padding = '20px';
+};
